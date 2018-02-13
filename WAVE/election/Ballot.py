4f60689b7f51eb41d888bc2ce3b316a3bf1d396a
Ankur Gupta <rukna1000@gmail.com>
2018-02-13 13:33:27 -0500

    Finished Ballot

--- WAVE/election/Ballot.py
+++ WAVE/election/Ballot.py
@@ -3,29 +3,29 @@ class Ballot:
         self._audit_seq_num = -1
         self._physical_ballot_num = -1
         self._reported_value = None
-        self.actual_value = None
+        self._actual_value = None
 
     def get_audit_seq_num(self):
-        pass
+        return self._audit_seq_num
 
-    def set_audit_seq_num(self):
-        pass
+    def set_audit_seq_num(self, audit_seq_num):
+        self._audit_seq_num = audit_seq_num
 
     def get_physical_ballot_num(self):
-        pass
+        return self._physical_ballot_num
 
-    def set_physical_ballot_num(self):
-        pass
+    def set_physical_ballot_num(self, physical_ballot_num):
+        self._physical_ballot_num = physical_ballot_num
 
     def get_reported_value(self):
-        pass
+        return self._reported_value
 
-    def set_reported_value(self):
-        pass
+    def set_reported_value(self, reported_value):
+        self._reported_value = reported_value
 
     def get_actual_value(self):
-        pass
+        return self._actual_value
 
-    def set_actual_value(self):
-        pass
+    def set_actual_value(self, actual_value):
+        self._actual_value = actual_value
 
