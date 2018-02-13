e7e8855e43908dfcec72015f4425d5b29a3b900c
Ankur Gupta <rukna1000@gmail.com>
2018-02-13 13:59:37 -0500

    Finished the Audit class

--- WAVE/audit/Audit.py
+++ WAVE/audit/Audit.py
@@ -3,3 +3,28 @@ from abc import ABC
 class Audit(ABC):
     @abstractmethod
     def get_progress(self):
+        pass
+
+    @abstractmethod
+    def get_status(self):
+        pass
+
+    @abstractmethod
+    def get_name(self):
+        pass
+
+    @abstractmethod
+    def get_parameters(self):
+        pass
+
+    @abstractmethod
+    def set_parameters(self, param):
+        pass
+
+    @abstractmethod
+    def recompute(self, ballot):
+        pass
+
+    @abstractmethod
+    def get_current_result(self):
+        pass
