47d7a2fa0ec4e2776411dfc07af01cefb3b0c42b
Ankur Gupta <rukna1000@gmail.com>
2018-02-13 13:21:11 -0500

    Added init method

--- WAVE/election/Election.py
+++ WAVE/election/Election.py
@@ -1,28 +1,34 @@
 class Election:
-    def get_contestants():
+    def __init__(self):
+        self._contestants = list()
+        self._reported_results = list()
+        self._actual_results = list()
+        self._ballots = list()
+
+    def get_contestants(self):
         pass
 
-    def set_contestants():
+    def set_contestants(self):
         pass
 
-    def get_reported_results():
+    def get_reported_results(self):
         pass
 
-    def set_reported_results():
+    def set_reported_results(self):
         pass
 
-    def get_actual_results():
+    def get_actual_results(self):
         pass
 
-    def set_actual_results():
+    def set_actual_results(self):
         pass
 
-    def get_ballots():
+    def get_ballots(self):
         pass
 
-    def set_ballots():
+    def set_ballots(self):
         pass
 
-    def add_ballot():
+    def add_ballot(self):
         pass
 
