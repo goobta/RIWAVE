41fd4ba405dc082ed5fa33f57ca04ad302c794a5
Ankur Gupta <rukna1000@gmail.com>
2018-02-13 13:25:39 -0500

    Completed Election

--- WAVE/election/Election.py
+++ WAVE/election/Election.py
@@ -6,29 +6,28 @@ class Election:
         self._ballots = list()
 
     def get_contestants(self):
-        pass
+        return self._contestants
 
-    def set_contestants(self):
-        pass
+    def set_contestants(self, contestants):
+        self._contestants = contestants
 
     def get_reported_results(self):
-        pass
+        return self._reported_results
 
-    def set_reported_results(self):
-        pass
+    def set_reported_results(self, reported_results):
+        self._reported_results = reported_results
 
     def get_actual_results(self):
-        pass
+        return self._actual_results
 
-    def set_actual_results(self):
-        pass
+    def set_actual_results(self, actual_results):
+        self._actual_results = actual_results
 
     def get_ballots(self):
-        pass
+        return self._ballots
 
-    def set_ballots(self):
-        pass
+    def set_ballots(self, ballots):
+        self._ballots = ballots
 
-    def add_ballot(self):
-        pass
-
+    def add_ballot(self, ballot):
+        self._ballots.append(ballot)
