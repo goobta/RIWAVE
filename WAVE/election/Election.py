b47da5e2be5ac16f5cfc079a16557b30ab175743
Ankur Gupta <rukna1000@gmail.com>
2018-02-13 13:10:58 -0500

    Created the base for Election

---
+++ WAVE/election/Election.py
@@ -0,0 +1,28 @@
+class Election:
+    def get_contestants():
+        pass
+
+    def set_contestants():
+        pass
+
+    def get_reported_results():
+        pass
+
+    def set_reported_results():
+        pass
+
+    def get_actual_results():
+        pass
+
+    def set_actual_results():
+        pass
+
+    def get_ballots():
+        pass
+
+    def set_ballots():
+        pass
+
+    def add_ballot():
+        pass
+
