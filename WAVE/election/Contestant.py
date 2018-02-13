7003750b14e19ff36e9b96587be3b1fa33c05f4b
Ankur Gupta <rukna1000@gmail.com>
2018-02-13 13:35:22 -0500

    Created the Contestant class

---
+++ WAVE/election/Contestant.py
@@ -0,0 +1,15 @@
+class Contestant:
+    def __init__(self):
+        pass
+
+    def get_id(self):
+        pass
+
+    def set_id(self):
+        pass
+
+    def get_name(self):
+        pass
+
+    def set_name(self):
+        pass
