624de6cfc31dd200a9069b616c12fe2997adb1fc
Ankur Gupta <rukna1000@gmail.com>
2018-02-13 13:29:43 -0500

    Added gitignore and Ballot

---
+++ WAVE/election/Ballot.py
@@ -0,0 +1,28 @@
+class Ballot:
+    def __init__(self):
+        pass
+
+    def get_audit_seq_num(self):
+        pass
+
+    def set_audit_seq_num(self):
+        pass
+
+    def get_physical_ballot_num(self):
+        pass
+
+    def set_physical_ballot_num(self):
+        pass
+
+    def get_reported_value(self):
+        pass
+
+    def set_reported_value(self):
+        pass
+
+    def get_actual_value(self):
+        pass
+
+    def set_actual_value(self):
+        pass
+
