6bc7f98c85d68bb2995248ab30d12c24a3965672
Ankur Gupta <rukna1000@gmail.com>
2018-02-14 15:47:25 -0500

    Worked on the 2016 presidential race

--- WAVE/data_gen/pres2016.py
+++ WAVE/data_gen/pres2016.py
@@ -1,3 +1,4 @@
+import math
 import election
 
 
@@ -6,6 +7,9 @@ class Pres2016:
         self._election = election.Election()
 
         self._gen_and_set_candidates()
+
+    def get_election(self):
+        return self._election()
 
     def _gen_and_set_candidates(self):
         self._contestants = []
@@ -20,14 +24,43 @@ class Pres2016:
         self._election.set_contestants(self._contestants)
 
     def _set_reported_results(self):
-       results = []
+        self._results = []
 
-       results.append(election.Result(self._contestants[0], .544))
-       results.append(election.Result(self._contestants[1], 0.389))
-       results.append(election.Result(self._contestants[2], 0.032))
-       results.append(election.Result(self._contestants[3], 0.02))
-       results.append(election.Result(self._contestants[4], 0.013))
-       results.append(election.Result(self._contestants[5], 0.001))
+        self._results.append(election.Result(self._contestants[0], .544))
+        self._results.append(election.Result(self._contestants[1], 0.389))
+        self._results.append(election.Result(self._contestants[2], 0.032))
+        self._results.append(election.Result(self._contestants[3], 0.02))
+        self._results.append(election.Result(self._contestants[4], 0.013))
+        self._results.append(election.Result(self._contestants[5], 0.001))
+        
+        self._election.set_reported_results(self._results)
 
-       self._election.set_reported_results(results)
+    def _gen_ballots(self, count, error):
+        sorted_results = sorted(self._results, 
+                                key=lambda r: r.get_percentage,
+                                reversed=True)
 
+        ballot_count = []
+
+        for i, result in enumerate(sorted_results):
+            if i == 0:
+                ballot_count.append(result.get_contestant(),
+                                    math.floor((result.get_percentage() + error)
+                                                * count))
+            
+            elif i == 1:
+                ballot_count.append(result.get_contestant(),
+                                    math.floor((result.get_percentage() - error)
+                                                * count))
+
+            else:
+                ballot_count.append(result.get_contestant(),
+                                    math.floor(result.get_percentage() * count))
+
+        true_count = 0
+
+        ballots = []
+
+        for batch in ballot_count:
+            for i in range(batch[1]):
+                
