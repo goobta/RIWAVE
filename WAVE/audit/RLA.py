2190c094850851835dc56b59b69a71ab5c3d3953
Ankur Gupta <rukna1000@gmail.com>
2018-02-13 19:21:42 -0500

    Created RLA file

---
+++ WAVE/audit/RLA.py
@@ -0,0 +1,24 @@
+import WAVE.audit as audits
+
+
+class RLA(audits.Audit):
+    def get_progress(self):
+        pass
+
+    def get_status(self):
+        pass
+
+    def get_name(self):
+        pass
+
+    def get_parameters(self):
+        pass
+
+    def set_parameters(self, param):
+        pass
+
+    def recompute(self, ballot):
+        pass
+
+    def get_current_result(self):
+        pass
